6e015428b607fd035b6c00adf9d58a0cd8e6e31d
Dibyani Sahu <sahudibyani006@gmail.com>
2022-10-31 17:00:15 +0100

    SQL labs1 and 2 Week3_D1

---
+++ SQLW3D1.sql
@@ -0,0 +1,8 @@
+use sakila;
+select * from sakila.film;
+select title,replacement_cost from sakila.film;
+select title,replacement_cost as cost_repl from sakila.film;
+SELECT distinct name as 'NAME' FROM sakila.language;
+select * from sakila.store;
+select  distinct staff_id from sakila.staff;
+select first_name from sakila.staff;
